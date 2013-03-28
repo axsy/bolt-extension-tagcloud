1719f277113decaa162c2cedf5367edecc9130a1
Aleksey Orlov <i.trancer@gmail.com>
2013-03-29 01:40:12 +0200

    Implemented view

--- extension.php
+++ extension.php
@@ -14,7 +14,7 @@ namespace TagCloud
         {
             $data = array(
                 'name' => "TagCloud",
-                'description' => "This extension includes tag-cloud generation and helpers to display these clouds",
+                'description' => "An extension provides capapbility of tag cloud generation and helpers to display these clouds",
                 'keywords' => "bolt, extension, tagcloud",
                 'author' => "Aleksey Orlov",
                 'link' => "https://github.com/axsy/bolt-extension-tagcloud",
@@ -45,6 +45,8 @@ namespace TagCloud\Provider
     use TagCloud\Engine\Builder;
     use TagCloud\Engine\Repository;
     use TagCloud\Engine\Storage;
+    use TagCloud\Engine\TwigExtension;
+    use TagCloud\Engine\View;
     use Bolt\StorageEvent;
     use Bolt\StorageEvents;
 
@@ -61,10 +63,15 @@ namespace TagCloud\Provider
             $app['tagcloud.storage'] = $app->share(function ($app) {
                 return new Storage($app['tagcloud.builder'], $app['cache']);
             });
+            $app['tagcloud.view'] = $app->share(function ($app) {
+                return new View($app['tagcloud.storage'], $app['paths']['root']);
+            });
 
             $app['dispatcher']->addListener(StorageEvents::postSave, function (StorageEvent $event) use ($app) {
                 $app['tagcloud.storage']->deleteCloud($event->getContent()->contenttype['slug']);
             });
+
+            $app['twig']->addExtension(new TwigExtension($app['tagcloud.view']));
         }
 
         public function boot(Application $app)
@@ -78,9 +85,15 @@ namespace TagCloud\Engine
     use TagCloud\Engine\Exception\NoTagsTaxonomiesAvailableException;
     use TagCloud\Engine\Exception\NoTaxonomiesAvailableException;
     use TagCloud\Engine\Exception\UnknownContentTypeException;
+    use TagCloud\Engine\Exception\UnsupportedViewException;
     use Doctrine\Common\Cache\CacheProvider;
     use Doctrine\DBAL\Connection;
+    use Bolt\Content;
     use PDO;
+
+    interface Exception
+    {
+    }
 
     class Storage
     {
@@ -107,17 +120,17 @@ namespace TagCloud\Engine
             return $cloud;
         }
 
+        protected function getKeyFor($contentType)
+        {
+            return 'tagcloud_' . $contentType;
+        }
+
         public function deleteCloud($contentType)
         {
             $key = $this->getKeyFor($contentType);
             if ($this->cache->contains($key)) {
                 $this->cache->delete($key);
             }
-        }
-
-        protected function getKeyFor($contentType)
-        {
-            return 'tagcloud_' . $contentType;
         }
     }
 
@@ -165,7 +178,6 @@ namespace TagCloud\Engine
          * @var array
          */
         protected $config;
-
         protected $repository;
 
         public function __construct(array $config, Repository $repository)
@@ -177,12 +189,10 @@ namespace TagCloud\Engine
         public function buildCloudFor($contentType)
         {
             if (!isset($this->config['contenttypes'][$contentType])) {
-                require_once __DIR__ . '/Exception/UnknownContentTypeException.php';
                 throw new UnknownContentTypeException($contentType);
             }
 
             if (!isset($this->config['contenttypes'][$contentType]['taxonomy'])) {
-                require_once __DIR__ . '/Exception/NoTaxonomiesAvailableException.php';
                 throw new NoTaxonomiesAvailableException($contentType);
             }
 
@@ -196,7 +206,6 @@ namespace TagCloud\Engine
                 }
             }
             if (is_null($behavesLikeTags)) {
-                require_once __DIR__ . '/Exception/NoTagsTaxonomiesAvailableException.php';
                 throw new NoTagsTaxonomiesAvailableException($contentType);
             }
 
@@ -211,7 +220,10 @@ namespace TagCloud\Engine
                 }
             }
 
-            return $tags;
+            return array(
+                'taxonomytype' => $behavesLikeTags,
+                'tags' => $tags
+            );
         }
 
         protected function normalize($rank, $maxRank)
@@ -220,8 +232,160 @@ namespace TagCloud\Engine
         }
     }
 
-    interface Exception
+    class View
     {
+        protected $storage;
+
+        public function __construct(Storage $storage, $baseUrl)
+        {
+            $this->storage = $storage;
+            $this->baseUrl = $baseUrl;
+        }
+
+        public function render($contentType, array $options)
+        {
+            $cloud = $this->storage->fetchCloud($contentType);
+
+            $options = array_merge_recursive($this->getDefaultOptions(), $options);
+
+            if ('raw' != $options['view']) {
+                $html = '<ul';
+                if (!empty($options['list_options']) && is_array($options['list_options'])) {
+                    $html .= $this->renderOptions($options['list_options']);
+                }
+                $html .= '>';
+            } else {
+                $html = null;
+            }
+            foreach ($cloud['tags'] as $tag => $rank) {
+                $link = $this->renderLink(
+                    $cloud['taxonomytype'], $tag, $rank, $options['marker'], $options['link_options']);
+                switch ($options['view']) {
+                    case 'raw':
+                        $html .= "$link ";
+                        break;
+                    case 'list':
+                        $html .= "<li>$link</li>";
+                        break;
+                    default:
+                        throw new UnsupportedViewException($options['view']);
+                        break;
+                }
+            }
+            $html = trim($html) . ('raw' != $options['view'] ? '</ul>' : null);
+
+            return $html;
+        }
+
+        public function getDefaultOptions()
+        {
+            return array(
+                'view' => 'list',
+                'marker' => 'tag-{rank}',
+                'list_options' => null,
+                'link_options' => null
+            );
+        }
+
+        private function renderOptions(array $options)
+        {
+            $html = null;
+            foreach($options as $key => $value) {
+                $html .= " $key=\"" . (is_array($value) ? implode(' ', $value) : trim($value)) . '"';
+            }
+            $html .= '>';
+            return $html;
+        }
+
+        private function renderLink($taxonomyType, $tag, $rank, $marker, array $options = null)
+        {
+            if (!isset($options['class'])) {
+                $options['class'] = null;
+            }
+            if (is_array($options['class'])) {
+                $options['class'] = implode(' ', $options['class']);
+            }
+            $options['class'] .= ' ' . str_replace('{rank}', $rank, $marker);
+
+            $html = '<a href="' . sprintf("%s%s/%s", $this->baseUrl, $taxonomyType, $tag) . '"'
+                . $this->renderOptions($options) . '>' . $tag . '</a>';
+
+            return $html;
+        }
+    }
+
+    class TwigExtension extends \Twig_Extension
+    {
+        protected $view;
+
+        public function __construct(View $view)
+        {
+            $this->view = $view;
+        }
+
+        public function getFunctions()
+        {
+            return array(
+                new \Twig_SimpleFunction('tag_cloud', array($this, 'render'), array('is_safe' => array('html'))),
+                new \Twig_SimpleFunction('tag_cloud_raw', array($this, 'renderRaw'), array('is_safe' => array('html'))),
+                new \Twig_SimpleFunction('tag_cloud_list', array($this, 'renderList'), array('is_safe' => array('html'))),
+            );
+        }
+
+        public function getFilters()
+        {
+            return array(
+                new \Twig_SimpleFilter('contenttype', array($this, 'getContentType'))
+            );
+        }
+
+        public function getContentType($content)
+        {
+            return $content instanceof Content ? $content->contenttype['slug'] : false;
+        }
+
+        public function render($contentType, array $options = null)
+        {
+            return $this->view->render($contentType, $options);
+        }
+
+        public function renderRaw($contentType, $linkOptions = null, $marker = null)
+        {
+            $options = array(
+                'view' => 'raw'
+            );
+            if (!is_null($linkOptions)) {
+                $options['link_options'] = $linkOptions;
+            }
+            if (!is_null($marker)) {
+                $options['marker'] = $marker;
+            }
+
+            return $this->render($contentType, $options);
+        }
+
+        public function renderList($contentType, $listOptions = null, $linkOptions = null, $marker = null)
+        {
+            $options = array(
+                'view' => 'list'
+            );
+            if (!is_null($listOptions)) {
+                $options['list_options'] = $listOptions;
+            }
+            if (!is_null($linkOptions)) {
+                $options['link_options'] = $linkOptions;
+            }
+            if (!is_null($marker)) {
+                $options['marker'] = $marker;
+            }
+
+            return $this->render($contentType, $options);
+        }
+
+        public function getName()
+        {
+            return 'tagcloud';
+        }
     }
 }
 
@@ -277,6 +441,23 @@ namespace TagCloud\Engine\Exception
         public function getContentType()
         {
             return $this->contentType;
+        }
+    }
+
+    class UnsupportedViewException extends \RuntimeException implements Exception
+    {
+        private $view;
+
+        public function __construct($view)
+        {
+            $this->view = $view;
+
+            parent::__construct(sprintf('Unknown view mode \'%s\'', $view));
+        }
+
+        public function getView()
+        {
+            return $this->view;
         }
     }
 }
